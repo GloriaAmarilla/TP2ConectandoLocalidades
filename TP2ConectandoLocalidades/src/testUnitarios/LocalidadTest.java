e6d21f05414a46aa76e5e0fdfcde8a1f1f4eaac3
Gloria Amarilla <bebooguzman@gmail.com>
2026-04-19 22:44:26 -0300

    merge

---
+++ TP2ConectandoLocalidades/src/testUnitarios/LocalidadTest.java
@@ -0,0 +1,16 @@
+package testUnitarios;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import logica.Localidad;
+
+public class LocalidadTest {
+
+	@Test (expected = IllegalArgumentException.class)
+	public void nombreYProvinciaVaciosTest() {
+		Localidad loc = new Localidad("","",3.4,5.6);
+	}
+
+}
